0f2464764b244aa2dc44537e834660294ff4958f
Ryan Liu <ryan@launchlab.com.au>
2019-11-05 16:46:58 +1100

    - Update netlify config

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import Auth from './Auth'
 
 const App: React.FC<{}> = () => {
-  console.log(process.env)
-  const url =
-    process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-      ? process.env.NETLIFY_URL!
-      : process.env.NETLIFY_PUBLIC_URL!
+  const url = process.env.NETLIFY_URL!
   return (
     <main>
       <IdentityContextProvider url={url}>
