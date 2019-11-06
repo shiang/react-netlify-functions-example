4fecd68fe4af1caf70fdcc27de8990e8701bb6b9
Ryan Liu <ryan@launchlab.com.au>
2019-11-06 16:27:44 +1100

    - Update webpack base config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.tsx',
@@ -29,6 +31,8 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
